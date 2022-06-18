8d8ee54817aa983cdf49953a707b7516c8c606ee
Kota Jotaki <jotaki@orchestrabit.com>
2022-06-18 21:19:23 +0900

    First Commit

---
+++ app/src/index.php
@@ -0,0 +1,15 @@
+<?php
+require './vendor/autoload.php';
+
+$smarty = new Smarty();
+
+$smarty->template_dir = 'templates/';
+$smarty->compile_dir  = 'templates_c/';
+
+$smarty->assign('test1', '1行目です。');
+$smarty->assign('test2', '2行目です。');
+$smarty->assign('test3', '3行目です。');
+
+$smarty->display('test.html');
+
+?>
