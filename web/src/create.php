28d0d4b9465670b445061347e08ec7280a395809
Kota Jotaki <jotaki@orchestrabit.com>
2022-06-19 15:41:45 +0900

    ゲスト登録を作成

--- web/src/create.php
+++ web/src/create.php
@@ -1,11 +1,29 @@
 <?php
-require './vendor/autoload.php';
+require './init.php';
 
-$smarty = new Smarty();
+var_dump($_POST);
 
-$smarty->template_dir = 'templates/';
-$smarty->compile_dir  = 'templates_c/';
+// DBへ接続
+try {
 
+  $dbh = new PDO($dsn, $db_user, $db_password);
+
+  // クエリの実行
+  $query = "INSERT INTO `guest` (`name`, `email`, `address`) VALUES (?, ?, ?)";
+  $stmt = $dbh->prepare($query);
+  $stmt->execute(array($_POST['Name'], $_POST['Email'], $_POST['Address']));
+
+} catch(PDOException $e) {
+
+  $smarty->assign('message', "データベースの接続に失敗しました　" . $e->getMessage());
+  $smarty->display('error.html');
+  die();
+
+}
+
+// DB接続を閉じる
+$dbh = null;
+
+$message = "ゲスト登録しました";
+$smarty->assign('message', $message);
 $smarty->display('create.html');
-
-?>
